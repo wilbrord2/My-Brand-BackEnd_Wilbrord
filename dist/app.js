686dd06c9608e1046429dac7e276678cbdd50c5a
Wilbrord <bwilbrord@gmail.com>
2023-02-04 14:47:32 +0200

    updates

--- dist/app.js
+++ dist/app.js
@@ -12,13 +12,10 @@ var _cors = _interopRequireDefault(require("cors"));
 var _dotenv = _interopRequireDefault(require("dotenv"));
 var _swaggerUiExpress = _interopRequireDefault(require("swagger-ui-express"));
 var _documentation = _interopRequireDefault(require("./helpers/documentation"));
-var _message = _interopRequireDefault(require("./routes/message"));
-var _user = _interopRequireDefault(require("./routes/user"));
-var _authverified = _interopRequireDefault(require("./routes/authverified"));
-var _blog = _interopRequireDefault(require("./routes/blog"));
+var _routes = _interopRequireDefault(require("./routes"));
 function cov_21gyyzflvb() {
   var path = "C:\\AA Projects\\Andela\\My Backend\\src\\app.js";
-  var hash = "93c504bd49997cc303a97890373964950a3a3095";
+  var hash = "d2895c7eb4a0c7d1a2ae6f85443078c45b5133c3";
   var global = new Function("return this")();
   var gcv = "__coverage__";
   var coverageData = {
@@ -26,85 +23,65 @@ function cov_21gyyzflvb() {
     statementMap: {
       "0": {
         start: {
-          line: 9,
+          line: 10,
           column: 13
         },
         end: {
-          line: 9,
+          line: 10,
           column: 37
         }
       },
       "1": {
         start: {
-          line: 10,
+          line: 11,
           column: 12
         },
         end: {
-          line: 10,
+          line: 11,
           column: 21
         }
       },
       "2": {
         start: {
-          line: 11,
+          line: 12,
           column: 0
         },
         end: {
-          line: 11,
+          line: 12,
           column: 16
         }
       },
       "3": {
         start: {
-          line: 19,
+          line: 17,
           column: 0
         },
         end: {
-          line: 19,
+          line: 17,
           column: 27
         }
       },
       "4": {
         start: {
-          line: 20,
+          line: 18,
           column: 0
         },
         end: {
-          line: 20,
-          column: 39
+          line: 22,
+          column: 2
         }
       },
       "5": {
         start: {
-          line: 21,
+          line: 23,
           column: 0
         },
         end: {
-          line: 21,
-          column: 31
+          line: 23,
+          column: 24
         }
       },
       "6": {
-        start: {
-          line: 22,
-          column: 0
-        },
-        end: {
-          line: 22,
-          column: 42
-        }
-      },
-      "7": {
-        start: {
-          line: 23,
-          column: 0
-        },
-        end: {
-          line: 23,
-          column: 39
-        }
-      },
-      "8": {
         start: {
           line: 24,
           column: 0
@@ -114,7 +91,7 @@ function cov_21gyyzflvb() {
           column: 44
         }
       },
-      "9": {
+      "7": {
         start: {
           line: 25,
           column: 0
@@ -124,133 +101,103 @@ function cov_21gyyzflvb() {
           column: 67
         }
       },
-      "10": {
+      "8": {
         start: {
-          line: 26,
-          column: 0
+          line: 28,
+          column: 18
         },
         end: {
-          line: 26,
-          column: 16
+          line: 37,
+          column: 1
+        }
+      },
+      "9": {
+        start: {
+          line: 29,
+          column: 2
+        },
+        end: {
+          line: 36,
+          column: 3
+        }
+      },
+      "10": {
+        start: {
+          line: 30,
+          column: 4
+        },
+        end: {
+          line: 30,
+          column: 38
         }
       },
       "11": {
         start: {
-          line: 29,
-          column: 0
+          line: 31,
+          column: 17
         },
         end: {
           line: 31,
-          column: 3
+          column: 66
         }
       },
       "12": {
         start: {
-          line: 30,
-          column: 2
+          line: 32,
+          column: 4
         },
         end: {
-          line: 30,
-          column: 25
+          line: 32,
+          column: 62
         }
       },
       "13": {
         start: {
           line: 34,
-          column: 18
+          column: 4
         },
         end: {
-          line: 43,
-          column: 1
+          line: 34,
+          column: 23
         }
       },
       "14": {
         start: {
           line: 35,
-          column: 2
+          column: 4
         },
         end: {
-          line: 42,
-          column: 3
+          line: 35,
+          column: 20
         }
       },
       "15": {
         start: {
-          line: 36,
-          column: 4
+          line: 40,
+          column: 0
         },
         end: {
-          line: 36,
-          column: 38
+          line: 44,
+          column: 3
         }
       },
       "16": {
         start: {
-          line: 37,
-          column: 17
+          line: 41,
+          column: 2
         },
         end: {
-          line: 37,
-          column: 66
+          line: 43,
+          column: 5
         }
       },
       "17": {
         start: {
-          line: 38,
+          line: 42,
           column: 4
         },
         end: {
-          line: 38,
-          column: 62
-        }
-      },
-      "18": {
-        start: {
-          line: 40,
-          column: 4
-        },
-        end: {
-          line: 40,
-          column: 23
-        }
-      },
-      "19": {
-        start: {
-          line: 41,
-          column: 4
-        },
-        end: {
-          line: 41,
-          column: 20
-        }
-      },
-      "20": {
-        start: {
-          line: 46,
-          column: 0
-        },
-        end: {
-          line: 50,
-          column: 3
-        }
-      },
-      "21": {
-        start: {
-          line: 47,
-          column: 2
-        },
-        end: {
-          line: 49,
-          column: 5
-        }
-      },
-      "22": {
-        start: {
-          line: 48,
-          column: 4
-        },
-        end: {
-          line: 48,
+          line: 42,
           column: 33
         }
       }
@@ -260,132 +207,108 @@ function cov_21gyyzflvb() {
         name: "(anonymous_0)",
         decl: {
           start: {
-            line: 29,
-            column: 13
+            line: 28,
+            column: 18
           },
           end: {
-            line: 29,
-            column: 14
+            line: 28,
+            column: 19
           }
         },
         loc: {
           start: {
-            line: 29,
-            column: 27
+            line: 28,
+            column: 30
           },
           end: {
-            line: 31,
+            line: 37,
             column: 1
           }
         },
-        line: 29
+        line: 28
       },
       "1": {
         name: "(anonymous_1)",
         decl: {
           start: {
-            line: 34,
-            column: 18
+            line: 40,
+            column: 17
           },
           end: {
-            line: 34,
-            column: 19
+            line: 40,
+            column: 18
           }
         },
         loc: {
           start: {
-            line: 34,
-            column: 30
+            line: 40,
+            column: 23
           },
           end: {
-            line: 43,
+            line: 44,
             column: 1
           }
         },
-        line: 34
+        line: 40
       },
       "2": {
         name: "(anonymous_2)",
         decl: {
           start: {
-            line: 46,
-            column: 17
-          },
-          end: {
-            line: 46,
-            column: 18
-          }
-        },
-        loc: {
-          start: {
-            line: 46,
-            column: 23
-          },
-          end: {
-            line: 50,
-            column: 1
-          }
-        },
-        line: 46
-      },
-      "3": {
-        name: "(anonymous_3)",
-        decl: {
-          start: {
-            line: 47,
+            line: 41,
             column: 19
           },
           end: {
-            line: 47,
+            line: 41,
             column: 20
           }
         },
         loc: {
           start: {
-            line: 47,
+            line: 41,
             column: 25
           },
           end: {
-            line: 49,
+            line: 43,
             column: 3
           }
         },
-        line: 47
+        line: 41
       }
     },
     branchMap: {
       "0": {
         loc: {
           start: {
-            line: 9,
+            line: 10,
             column: 13
           },
           end: {
-            line: 9,
+            line: 10,
             column: 37
           }
         },
         type: "binary-expr",
         locations: [{
           start: {
-            line: 9,
+            line: 10,
             column: 13
           },
           end: {
-            line: 9,
+            line: 10,
             column: 29
           }
         }, {
           start: {
-            line: 9,
+            line: 10,
             column: 33
           },
           end: {
-            line: 9,
+            line: 10,
             column: 37
           }
         }],
-        line: 9
+        line: 10
       }
     },
     s: {
@@ -406,24 +329,18 @@ function cov_21gyyzflvb() {
       "14": 0,
       "15": 0,
       "16": 0,
-      "17": 0,
-      "18": 0,
-      "19": 0,
-      "20": 0,
-      "21": 0,
-      "22": 0
+      "17": 0
     },
     f: {
       "0": 0,
       "1": 0,
-      "2": 0,
-      "3": 0
+      "2": 0
     },
     b: {
       "0": [0, 0]
     },
     _coverageSchema: "1a1c01bbd47fc00a2c39e90264f33305004495a9",
-    hash: "93c504bd49997cc303a97890373964950a3a3095"
+    hash: "d2895c7eb4a0c7d1a2ae6f85443078c45b5133c3"
   };
   var coverage = global[gcv] || (global[gcv] = {});
   if (!coverage[path] || coverage[path].hash !== hash) {
@@ -444,60 +361,49 @@ const app = (cov_21gyyzflvb().s[1]++, (0, _express.default)());
 cov_21gyyzflvb().s[2]++;
 _dotenv.default.config();
 // ROUTES
-cov_21gyyzflvb().s[3]++;
+
 // Middlewares
 // bodyParser
+cov_21gyyzflvb().s[3]++;
 app.use(_bodyParser.default.json());
 cov_21gyyzflvb().s[4]++;
-app.use("/api/messages", _message.default);
+app.use((0, _cors.default)({
+  origin: "*"
+}));
 cov_21gyyzflvb().s[5]++;
-app.use("/api/user", _user.default);
+app.use("/api", _routes.default);
 cov_21gyyzflvb().s[6]++;
-app.use("/api/access", _authverified.default);
-cov_21gyyzflvb().s[7]++;
-app.use("/api/article", _blog.default);
-cov_21gyyzflvb().s[8]++;
 app.use("/documentation", _swaggerUiExpress.default.serve);
-cov_21gyyzflvb().s[9]++;
+cov_21gyyzflvb().s[7]++;
 app.use("/documentation", _swaggerUiExpress.default.setup(_documentation.default));
-cov_21gyyzflvb().s[10]++;
-app.use((0, _cors.default)());
-
-// ROUTES
-cov_21gyyzflvb().s[11]++;
-app.get("/", (req, res) => {
-  cov_21gyyzflvb().f[0]++;
-  cov_21gyyzflvb().s[12]++;
-  res.send(" home-Page");
-});
 
 //CONNECT TO db
-cov_21gyyzflvb().s[13]++;
+cov_21gyyzflvb().s[8]++;
 const connectDB = async () => {
-  cov_21gyyzflvb().f[1]++;
-  cov_21gyyzflvb().s[14]++;
+  cov_21gyyzflvb().f[0]++;
+  cov_21gyyzflvb().s[9]++;
   try {
-    cov_21gyyzflvb().s[15]++;
+    cov_21gyyzflvb().s[10]++;
     _mongoose.default.set("strictQuery", true);
-    const conn = (cov_21gyyzflvb().s[16]++, await _mongoose.default.connect(process.env.DB_CONNECTION));
-    cov_21gyyzflvb().s[17]++;
+    const conn = (cov_21gyyzflvb().s[11]++, await _mongoose.default.connect(process.env.DB_CONNECTION));
+    cov_21gyyzflvb().s[12]++;
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    cov_21gyyzflvb().s[18]++;
+    cov_21gyyzflvb().s[13]++;
     console.log(error);
-    cov_21gyyzflvb().s[19]++;
+    cov_21gyyzflvb().s[14]++;
     process.exit(1);
   }
 };
 
 //Connect to the database before listening
-cov_21gyyzflvb().s[20]++;
+cov_21gyyzflvb().s[15]++;
 connectDB().then(() => {
-  cov_21gyyzflvb().f[2]++;
-  cov_21gyyzflvb().s[21]++;
+  cov_21gyyzflvb().f[1]++;
+  cov_21gyyzflvb().s[16]++;
   app.listen(PORT, () => {
-    cov_21gyyzflvb().f[3]++;
-    cov_21gyyzflvb().s[22]++;
+    cov_21gyyzflvb().f[2]++;
+    cov_21gyyzflvb().s[17]++;
     console.log("connect to DB");
   });
 });
