056a90d2ecb573eb5a26958f6981e8fdb9b66bff
Wilbrord <bwilbrord@gmail.com>
2023-02-07 13:15:24 +0200

    returning json response

--- src/app.js
+++ src/app.js
@@ -13,12 +13,19 @@ dotenv.config();
 // Middlewares
 // bodyParser
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: "http://127.0.0.1:5500",
-    optionsSuccessStatus: 200
-  })
-);
+
+var allowedOrigins = ['http://127.0.0.1:5500']
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+  },
+  optionsSuccessStatus: 200
+}))
 app.use("/api", routes);
 app.use("/documentation", swaggerDoc.serve);
 app.use("/documentation", swaggerDoc.setup(swaggerDocumentations));
